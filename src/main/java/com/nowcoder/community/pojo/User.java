e2560352e04b001a8be86b6ab9b0cdee5c0b3f5a
Munich <2493929776@qq.com>
2023-06-18 09:22:57 +0800

    register and login

--- src/main/java/com/nowcoder/community/pojo/User.java
+++ src/main/java/com/nowcoder/community/pojo/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
@@ -21,5 +22,5 @@ public class User {
     private Integer status;
     private String activationCode;
     private String headerUrl;
-    private Date createTime;
+    private LocalDateTime createTime;
 }
